3a95548b92b89a26a3993bdb63a2fdf0353e7ca2
Andy <walshrandrew@gmail.com>
2025-11-20 14:05:48 -0700

    asdfasdf

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,49 @@ app.get('/Albums', async (req, res) => {
 
 });
 
+// CREATE Albums
+app.post('/Albums/create', async (req, res) => {
+    try {
+        const data = req.body;
+        const query1 = 'CALL sp_CreateALbum(?, ?, ?, ?, ?);';
+        await db.query(query1, [
+            data.albumName,
+            data.albumPrice,
+            data.amountInStock,
+            data.artistID,
+            data.genreID
+        ]);
+
+        console.log(`Created Album: ${data.albumName}`);
+        res.redirect('/Albums');
+    } catch(error) {
+        console.error('Error executing create albums queries:', error);
+        res.status(500).send('An error occurred while creating a new album.');
+    }
+});
+
+// UPDATE Albums
+app.post('/Albums/udpate', async (req, res) => {
+    try {
+        const data = req.body;
+        const query1 = 'CALL sp_UpdateAlbum(?, ?, ?, ?, ?, ?);';
+        await db.query(query1, [
+            data.ablumID,
+            data.albumName,
+            data.albumPrice,
+            data.amountInStock,
+            data.artistID,
+            data.genreID
+        ]);
+        console.log(`Updated Album.id: ${data.albumID}`);
+        res.redirect('/Albums');
+
+    } catch(error) {
+        console.error('Error executing queries', error);
+        res.status(500).send('An Error occurred while updating the album.');
+    }
+});
+
 // DELETE Albums
 app.post('/Albums/delete', async (req, res) => {
     try {
